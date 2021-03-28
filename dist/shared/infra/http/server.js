ad8e3df31efc9e51f3ed8f808a5f8a51d0473bdd
Jose Martins <jose.melo@doutorhoje.com>
2021-03-28 17:49:02 -0300

    ormconfig

--- dist/shared/infra/http/server.js
+++ dist/shared/infra/http/server.js
@@ -34,6 +34,6 @@ app.use((err, request, response, _) => {
     message: 'Internal server error'
   });
 });
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log('🚀 Server started on port 3000!!');
 });
