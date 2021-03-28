76b9492cab113aeeb265d6b64420611d63b8842a
Jose Martins <jose.melo@doutorhoje.com>
2021-03-28 18:27:12 -0300

    subindo alteracoes

--- ormconfig.js
+++ ormconfig.js
@@ -4,7 +4,7 @@ module.exports = {
   url: process.env.DATABASE_URL,
   synchronize: false,
   logging: true,
-  extra: { ssl: true, rejectUnauthorized: false },
+  extra: process.env.TYPEORM_DRIVER_EXTRA,
   entities: [
     "./dist/modules/**/infra/typeorm/entities/*.js"
   ],
