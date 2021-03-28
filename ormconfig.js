d1ae5bb28170640221c6ecc5b2ea6450a3f0e03d
Jose Martins <jose.melo@doutorhoje.com>
2021-03-28 17:27:00 -0300

    ormconfig

--- ormconfig.js
+++ ormconfig.js
@@ -1,17 +1,17 @@
 
 module.exports = {
-  "type": "postgres",
-  "url": process.env.DATABASE_URL,
+  type: "postgres",
+  url: process.env.DATABASE_URL,
   extra: {
     ssl: true
   },
-  "entities": [
+  entities: [
     "./dist/modules/**/infra/typeorm/entities/*.js"
   ],
-  "migrations": [
+  migrations: [
     "./dist/shared/infra/typeorm/migrations/*.js"
   ],
-  "cli": {
-  "migrationsDir": "./dist/shared/infra/typeorm/migrations"
+  cli: {
+  migrationsDir: "./dist/shared/infra/typeorm/migrations"
   }
 }
